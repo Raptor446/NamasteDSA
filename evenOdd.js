ed1bfd6e249e610d0de72cec4f6f1771fc1b3d79
ayush jaiswal <ayushjaiswal326@gmail.com>
2025-12-14 19:29:21 +0530

    Check Even Odd

---
+++ evenOdd.js
@@ -0,0 +1,11 @@
+function isEvenOrOdd (num) {
+    if (num % 2 === 0) {
+        console.log("Even Number")
+    } else {
+        console.log("Odd Number")
+    }
+}
+
+isEvenOrOdd(5)
+isEvenOrOdd(8)
+isEvenOrOdd(-2)
