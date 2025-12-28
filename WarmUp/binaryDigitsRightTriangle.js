98548d1888b6ca3af941e6612af27e9e1237de6e
ayush jaiswal <ayushjaiswal326@gmail.com>
2025-12-28 17:33:40 +0530

    Star Patterns

---
+++ WarmUp/binaryDigitsRightTriangle.js
@@ -0,0 +1,13 @@
+let n = 6
+
+for (let i = 1; i <= n; i++) {
+    let row = ""
+    for (let j = 1; j <= i; j++) {
+        if (j % 2 === 0) {
+            row += 0
+        } else {
+            row += 1
+        }
+    }
+    console.log(row)
+}
--- WarmUp/starSquarePattern.js
+++ WarmUp/starSquarePattern.js
@@ -1,4 +1,5 @@
-let n = 4
+let n = 6
+
 for (let i = 0; i < n; i++) {
   let row = ""
   for (let j = 0; j < n; j++) {
